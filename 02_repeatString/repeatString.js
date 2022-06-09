caac73b55a2ba0d76d3be3ffa7b8b6b334d51cc0
david <boominbkim@gmail.com>
2022-06-08 21:51:19 -0400

    finish first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let ans = "";
+    if (num < 0) return 'ERROR';
+    while (num > 0){
+        ans = `${ans}${string}`;
+        num--;
+    }
+    return ans;
 };
 
 // Do not edit below this line
