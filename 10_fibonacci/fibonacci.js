902c3dd3d19f2e556ab59050b8d07de706637ab4
david <boominbkim@gmail.com>
2022-07-06 11:36:09 -0400

    finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+let nums = [1, 1];
 
+const fibonacci = function(x) {
+    if (typeof x === 'string'){
+        const num = Number(x);
+        if (!(Number.isInteger(num) && num > 0)) {
+            return "OOPS";
+        }
+        x = num;
+    }
+    if (x < 1) return "OOPS";
+    fillNums(x);
+    return nums[x-1];
 };
+
+const fillNums = (x) => {
+    while (nums.length < x) {
+        let sum = nums[nums.length-1] + nums[nums.length-2];
+        nums.push(sum);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
